5a2e0f76b738df83fb7b2bad62e0632261b9f7c5
ZthStudy <30645249+ZthStudy@users.noreply.github.com>
2021-01-05 11:15:29 +0800

    增加vue options的类型

---
+++ src/shims.d.ts
@@ -0,0 +1,13 @@
+/*
+ * @Description: 
+ * @version: 1.0.0
+ * @Author: zhangtianhou
+ * @Date: 2021-01-05 10:49:42
+ * @LastEditors: zhangtianhou
+ * @LastEditTime: 2021-01-05 11:04:49
+ */
+declare module '*.vue' {
+  import { ComponentOptions } from 'vue'
+  const componentOptions: ComponentOptions
+  export default componentOptions
+}
