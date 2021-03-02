025486a9963dcdeb86fede22b330068ad1848167
ZthStudy <30645249+ZthStudy@users.noreply.github.com>
2021-03-02 14:04:44 +0800

    引入ant-design

--- src/components/Main/JsList/index.vue
+++ src/components/Main/JsList/index.vue
@@ -4,25 +4,39 @@
  * @Author: zhangtianhou
  * @Date: 2021-02-15 11:34:24
  * @LastEditors: zhangtianhou
- * @LastEditTime: 2021-02-21 16:49:04
+ * @LastEditTime: 2021-02-23 19:28:54
 -->
 <template>
   <div>jslist</div>
-  <div id="editor"></div>
+  <a-input class="mb10" v-model:value="articleTitle" placeholder="输入文章标题">
+    <template #prefix>
+      <span class="mr10">文章标题</span>
+    </template>
+  </a-input>
+  <div id="wangEditor"></div>
+  <a-button type="primary" @click="submit">提交</a-button>
 </template>
 <script lang='ts'>
-import { defineComponent, onMounted } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 import E from "wangeditor";
 export default defineComponent({
   name: "JsList",
   components: {},
   props: [],
   setup: () => {
+    let editor: any = null;
     onMounted(() => {
-      console.log(1);
-      const editor = new E("#editor");
+      editor = new E("#wangEditor");
       editor.create();
     });
+    const articleTitle = ref<string>("");
+    const submit = () => {
+      console.log({ articleTitle, content: editor.txt.html() });
+    };
+    return {
+      articleTitle,
+      submit,
+    };
   },
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { createApp } from "vue";
 import APP from "./APP.vue";
 import router from "./router";
 import "./styles/index.scss";
+import "ant-design-vue/dist/antd.css";
+import { Input, Button } from "ant-design-vue";
 
 const app = createApp(APP);
 
 app.use(router);
+app.use(Input);
+app.use(Button);
 
 app.mount("#app");
