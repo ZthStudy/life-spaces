72b47b0ff4c585a9efb1520262e706c1c4831fe8
ZthStudy <30645249+ZthStudy@users.noreply.github.com>
2021-03-05 14:34:34 +0800

    test

--- src/components/Main/Write/index.vue
+++ src/components/Main/Write/index.vue
@@ -4,7 +4,7 @@
  * @Author: zhangtianhou
  * @Date: 2021-03-03 15:09:55
  * @LastEditors: zhangtianhou
- * @LastEditTime: 2021-03-03 17:25:39
+ * @LastEditTime: 2021-03-05 10:50:31
 -->
 <template>
   <a-space>
@@ -17,38 +17,45 @@
         <span class="mr10">文章标题</span>
       </template>
     </a-input>
-    <a-select v-model:value="sortValue">
-      <a-select-option value="js">js</a-select-option>
-      <a-select-option value="css">css</a-select-option>
-      <a-select-option value="travel">travel</a-select-option>
-      <a-select-option value="note">note</a-select-option>
-    </a-select>
+    <a-input v-model:value="sortValue" placeholder="输入分类">
+      <template #prefix>
+        <span class="mr10">分类</span>
+      </template>
+    </a-input>
   </a-space>
   <div id="wangEditor"></div>
-  <a-button type="primary" @click="submit">提交</a-button>
+  <a-button type="primary" @click="submit(articleTitle, sortValue)"
+    >提交</a-button
+  >
 </template>
 <script lang='ts'>
 import { defineComponent, onMounted, ref } from "vue";
-import { Input, Select, Button } from "ant-design-vue";
+// import { Input, Select, Button } from "ant-design-vue";
 import E from "wangeditor";
+import axios from "axios";
 export default defineComponent({
   name: "Write",
-  components: { Input, Select, Button },
+  // components: { Input, Select, Button },
   props: [],
-  setup: () => {
-    let editor: any = null;
+  setup() {
+    let wangEditor: any;
     onMounted(() => {
-      editor = new E("#wangEditor");
-      editor.create();
+      wangEditor = new E("#wangEditor");
+      wangEditor.create();
     });
-    const articleTitle = ref("");
-    const sortValue = ref("js");
-    const submit = () => {
-      console.log({ articleTitle, sortValue, content: editor.txt.html() });
+    const submit = (articleTitle: string, sortValue: string): void => {
+      console.log({ articleTitle, sortValue, content: wangEditor.txt.html() });
+      const res = axios.post("http://localhost:8000/api/blog", {
+        articleTitle,
+        sortValue,
+        content: wangEditor.txt.html(),
+      });
+      console.log({ res });
     };
+
     return {
-      articleTitle,
-      sortValue,
+      articleTitle: ref(""),
+      sortValue: ref("js"),
       submit,
     };
   },
