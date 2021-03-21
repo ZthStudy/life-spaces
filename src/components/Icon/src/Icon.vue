c070302040d4e44dbc4438ca1e797c62549a4df7
ZthStudy <814331049@qq.com>
2021-03-21 23:09:17 +0800

    多语言选择器

---
+++ src/components/Icon/src/Icon.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Description: 
+ * @version: 1.0.0
+ * @Author: zhangtianhou
+ * @Date: 2021-03-21 15:03:40
+ * @LastEditors: zhangtianhou
+ * @LastEditTime: 2021-03-21 15:03:40
+-->
+<template>
+  <span
+    ref="elRef"
+    :class="[$attrs.class, 'app-iconify anticon']"
+    :style="getWrapStyle"
+  ></span>
+</template>
+<script lang='ts'>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "",
+  components: {},
+  props: {
+    
+  },
+  setup: () => {},
+});
+</script>
+<style scoped>
+</style>
